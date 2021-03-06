32f8ac3df7a20fee5836bfecc5bcfeefe926574e
vuchungdung <vuchungdunghlym@gmail.com>
2021-03-06 12:28:02 +0700

    update code

--- UTEHY.WebApp/Controllers/PostController.cs
+++ UTEHY.WebApp/Controllers/PostController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
 using UTEHY.Model.Dtos;
+using UTEHY.Model.Entities;
 using UTEHY.Model.Enums;
 using UTEHY.Model.ViewModel;
 using UTEHY.Service.Interfaces;
@@ -15,10 +16,16 @@ namespace UTEHY.WebApp.Controllers
     public class PostController : Controller
     {
         private IPostService _postService;
+        private IPostCategoryService _postCategoryService;
+        private readonly FITDbContext _db;
 
-        public PostController(IPostService postService)
+        public PostController(IPostService postService, 
+            IPostCategoryService postCategoryService,
+            FITDbContext db)
         {
             _postService = postService;
+            _postCategoryService = postCategoryService;
+            _db = db;
         }
 
         public ActionResult Index()
@@ -26,6 +33,25 @@ namespace UTEHY.WebApp.Controllers
             var listItem = _postService.GetAll();
             return View(listItem);
         }
-        
+        public ActionResult Category()
+        {
+            var listItem = _postCategoryService.GetAll();
+            return PartialView(listItem);
+        }
+        public ActionResult PostRelate(string categoryId)
+        {
+            var listItem = _db.Posts.Select(x => new PostViewModel()
+            {
+                Name = x.Name,
+                ID = x.PostId,
+                Img = x.Img,
+                CreateDate = x.CreateDate
+            }).OrderBy(x=>x.CreateDate).ToList();
+            if (!String.IsNullOrEmpty(categoryId))
+            {
+                listItem = listItem.Where(x => x.CategoryId == categoryId).ToList();
+            }
+            return PartialView(listItem);
+        }
     }
 }
